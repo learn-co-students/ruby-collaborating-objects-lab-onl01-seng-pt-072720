bbb300eaf079a9302d16b114e177ca56c4607ce0
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-21 16:34:00 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+class Artist
+attr_accessor :name
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(title)
+    title.artist = self
+  end
+
+  def songs
+    Song.all.select { |search| search.artist == self }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.all.find { |artist| artist.name == name } || self.new(name)
+    # if self.all.find { |artist| artist.name == name }
+    #   self.all.find { |artist| artist.name == name }
+    # else
+    #   self.new(name)
+    # end
+  end
+
+  def print_songs
+    self.songs.each { |song| puts song.name  }
+  end
+
+end
